6b0f16ad537fb3129a09e115ea533867096bdbd9
i-am-awejshaikh <awejshaikh.work@gmail.com>
2026-04-18 22:53:27 +0530

    checking

--- src/main/java/com/EmployeeManagement/EmployeeManagementApplication.java
+++ src/main/java/com/EmployeeManagement/EmployeeManagementApplication.java
@@ -13,6 +13,7 @@ public class EmployeeManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 		logger.info("Alhamdulillah Rest Apis Machine Started");
+		logger.info("All Good");
 	}
 
 }
